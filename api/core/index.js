b206a0c5a4f7c356fcc071b68db840e5279579d6
Willerson Stockler <willersonsp@hotmail.com>
2023-02-14 14:50:15 -0300

    test.

--- api/core/index.js
+++ api/core/index.js
@@ -1,1 +1,18 @@
-module.exports = require('./src/index.js');
+//module.exports = require('./src/index.js');
+
+const fastify = require('fastify');
+
+function init() {
+  const app = fastify();
+  app.get('/', (request, reply) => reply.send({ hello: 'world' }));
+  return app;
+}
+
+if (require.main === module) {
+  init().listen({ port: 3000 }, (err) => {
+    if (err) console.error(err);
+    console.log('server listening on http://localhost:3000');
+  });
+} else {
+  module.exports = init
+}
