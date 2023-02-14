b134b5301891be8be9f06a23da13fd2885984e04
Willerson Stockler <willersonsp@hotmail.com>
2023-02-14 18:45:02 -0300

    refactor.

--- api/core/src/index.ts
+++ api/core/src/index.ts
@@ -3,29 +3,11 @@ import { FastifyFramework } from '@h4ad/serverless-adapter/lib/frameworks/fastif
 import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/lib/adapters/aws';
-import fastify from 'fastify';
+import app from './server';
 
-const app = fastify();
-
-app.get('/', async (request, reply) => {
-    return { Hello: "World" };
-})
-
-let defaultHandler: any = app.listen({ port: 3000 }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-})
-
-if (require.main !== module) {
-    defaultHandler = ServerlessAdapter.new(app)
-        .setFramework(new FastifyFramework())
-        .setHandler(new DefaultHandler())
-        .setResolver(new PromiseResolver())
-        .addAdapter(new ApiGatewayV2Adapter())
-        .build();
-}
-
-export const handler = defaultHandler;
+export const handler = ServerlessAdapter.new(app)
+    .setFramework(new FastifyFramework())
+    .setHandler(new DefaultHandler())
+    .setResolver(new PromiseResolver())
+    .addAdapter(new ApiGatewayV2Adapter())
+    .build();
