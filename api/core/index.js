ddb768665402636d30548df6efdbc4daeaa7a86b
Willerson Stockler <willersonsp@hotmail.com>
2023-02-14 16:35:24 -0300

    refactor.

--- api/core/index.js
+++ api/core/index.js
@@ -1,10 +1,1 @@
-//Only runs on AWS
-
-const awsLambdaFastify = require('@fastify/aws-lambda')
-const app = require('./src/index')
-
-const proxy = awsLambdaFastify(app, {
-    decorateRequest: false
-});
-
-exports.handler = proxy
+module.exports = require('./dist/index.js');
