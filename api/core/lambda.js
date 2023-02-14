702d9ea4f7952122d57139c22fe7c27583538885
Willerson Stockler <willersonsp@hotmail.com>
2023-02-14 10:52:23 -0300

    refactor.

---
+++ api/core/lambda.js
@@ -0,0 +1,5 @@
+const awsLambdaFastify = require('@fastify/aws-lambda')
+const app = require('./app')
+
+const proxy = awsLambdaFastify(app)
+exports.handler = proxy
--- api/core/src/index.js
+++ api/core/src/index.js
@@ -1,5 +1,4 @@
 const fastify = require('fastify');
-const awsLambdaFastify = require('@fastify/aws-lambda')
 
 function init() {
   const app = fastify();
@@ -8,13 +7,10 @@ function init() {
 }
 
 if (require.main === module) {
-  // called directly i.e. "node app"
   init().listen({ port: 3000 }, (err) => {
     if (err) console.error(err);
     console.log('server listening on http://localhost:3000');
   });
 } else {
-  // required as a module => executed on aws lambda
-  const proxy = awsLambdaFastify(init())
-  module.exports = proxy;
+  module.exports = app
 }
